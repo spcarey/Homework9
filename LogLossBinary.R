0e70e4842404fb0cdfa4719f9cc02c392e7675f2
Sean carey <scarey9@gmail.com>
2018-11-10 14:18:09 -0500

    * changed name of Homework_9 RMD due to an odd error with the space or underscore when kniting to html
    *finished homework

---
+++ LogLossBinary.R
@@ -0,0 +1,4 @@
+LogLossBinary = function(actual, predicted, eps = 1e-15) {  
+  predicted = pmin(pmax(predicted, eps), 1-eps)  
+  - (sum(actual * log(predicted) + (1 - actual) * log(1 - predicted))) / length(actual)
+}
